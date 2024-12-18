2926c68a94e255e1e81a407ae8f2855f7cfb9933
Aram Hagen <aram.hagen@gmail.com>
2024-12-18 07:25:53 -0800

    fix

--- mynode.js
+++ mynode.js
@@ -5,18 +5,23 @@ const checkSign = "\u{2705}";
 const dotenv = require("dotenv").config({ path: "src/.env" });
 
 const envFile = `export const environment = {
-    FIREBASE_API_KEY: '${process.env.FIREBASE_API_KEY}',
-    FIREBASE_AUTH_DOMAIN: '${process.env.FIREBASE_AUTH_DOMAIN}',
-    FIREBASE_PROJECT_ID: '${process.env.FIREBASE_PROJECT_ID}',
-    FIREBASE_STORAGE_BUCKET: '${process.env.FIREBASE_STORAGE_BUCKET}',    
-    FIREBASE_MESSAGING_SENDER_ID: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-    FIREBASE_APP_ID: '${process.env.FIREBASE_APP_ID}',
+    production: false,
+    firebaseConfig: {
+        apiKey: '${process.env.FIREBASE_API_KEY}',
+        authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+        projectId: '${process.env.FIREBASE_PROJECT_ID}',
+        storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+        messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+        appId: '${process.env.FIREBASE_APP_ID}'
+    }
 };
 `;
+
 const targetPath = path.join(
   __dirname,
   "./src/environments/environment.development.ts"
 );
+
 fs.writeFile(targetPath, envFile, (err) => {
   if (err) {
     console.error(err);
